ff09160d47e358dcc88259f98e63717125f39f79
Sean Hoffman <sean-hoffman@pluralsight.com>
2019-10-01 14:37:05 -0600

    Modules 2 and 3

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -30,10 +30,26 @@ func ListWinners(res http.ResponseWriter, req *http.Request) {
 
 // AddNewWinner adds new winner to the list
 func AddNewWinner(res http.ResponseWriter, req *http.Request) {
-
+	accessToken := req.Header.Get("X-ACCESS-TOKEN")
+	isTokenValid := data.IsAccessTokenValid(accessToken)
+	if !isTokenValid {
+		res.WriteHeader(http.StatusUnauthorized)
+	}
+	err := data.AddNewWinner(req.Body)
+	if err != nil {
+		res.WriteHeader(http.StatusUnprocessableEntity)
+	}
+	res.WriteHeader(http.StatusCreated)
 }
 
 // WinnersHandler is the dispatcher for all /winners URL
 func WinnersHandler(res http.ResponseWriter, req *http.Request) {
-
+	switch req.Method {
+	case http.MethodGet:
+		ListWinners(res, req)
+	case http.MethodPost:
+		AddNewWinner(res, req)
+	default:
+		res.WriteHeader(http.StatusMethodNotAllowed)
+	}
 }
